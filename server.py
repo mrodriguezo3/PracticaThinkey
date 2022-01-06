f3df837072fd77e43d716fd7592b6a192a700bec
mrodriguezo3 <97114192+mrodriguezo3@users.noreply.github.com>
2022-01-06 16:59:50 -0300

    Add files via upload
    Se supone que el Ejercicio Main llama al archivo security que a su vez llama al archivo user, el archivo debería ser capaz de loggear un usuario (bob) que está dentro del archivo security junto con los parametros de id(1) y su contraseña (asdf).

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+server = Flask(__name__)
+
+@server.route("/")
+def hello():
+   return "Hello World from Diego and Miguel!"
+
+if __name__ == "__main__":
+   server.run(host='0.0.0.0')
+
+
+
